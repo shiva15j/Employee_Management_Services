fed1d45d5185b075300ee61ec7fe1ae61ac200c8
Shivam <shivamsri15j@gmail.com>
2022-11-11 20:07:29 +0530

    new changes

--- React_Java/src/main/java/React_Java/React_Java/controller/EmployeeController.java
+++ React_Java/src/main/java/React_Java/React_Java/controller/EmployeeController.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import React_Java.React_Java.model.Employee;
 import React_Java.React_Java.repository.Employeerepository;
-
 
 //if there is error of cross origin in the console then we have to use this annotation
 @CrossOrigin(origins = "*")
@@ -21,12 +22,20 @@ public class EmployeeController {
 
 	@Autowired
 	private Employeerepository employeeRepository;
-	
+
 	@GetMapping("/employees")
-	public List<Employee> getAllEmployees(){
-		
+	public List<Employee> getAllEmployees() {
+
 		return employeeRepository.findAll();
 	}
-	
-	
+
+	// create employee rest api
+
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+
+		return employeeRepository.save(employee);
+
+	}
+
 }
